001ab357c891e12c44c1bc820ce61321eb6c6eb7
Chris Mwanya <cmwanya@gmail.com>
2024-08-04 16:09:29 +0100

    feat: move structure to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import Header from '@/components/header/header';
+import Logo from '@/components/logo/logo';
+import { ModeToggle } from '@/components/mode-toggle';
 import '@/styles/globals.css';
 import { ThemeProvider } from '@/theme-provider';
 
@@ -8,16 +11,24 @@ export default function RootLayout({
 }: React.PropsWithChildren<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <body>
+          <Header>
+            <Logo
+              color={{ primary: '', secondary: '' }}
+              height="3rem"
+              width="3rem"
+            />
+            <ModeToggle />
+          </Header>
+          <main className="container"> {children}</main>
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,5 @@
-import Header from '@/components/header/header';
-import Logo from '@/components/logo/logo';
-import { ModeToggle } from '@/components/mode-toggle';
-
 const Home = () => {
-  return (
-    <main className="">
-      <Header>
-        <Logo
-          color={{ primary: '', secondary: '' }}
-          height="3rem"
-          width="3rem"
-        />
-        <ModeToggle />
-      </Header>
-      hello
-    </main>
-  );
+  return <main className="">hello</main>;
 };
 
 export default Home;
