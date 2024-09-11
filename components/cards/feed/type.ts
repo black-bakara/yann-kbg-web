283051d111adcd39fd26f5cfe5e5d173cbb8105d
Chris Mwanya <cmwanya@gmail.com>
2024-09-11 11:07:46 +0100

    ref: remove unused code

--- components/cards/feed/type.ts
+++ components/cards/feed/type.ts
@@ -1,19 +1,5 @@
 import type { Feed } from '@/graphql/graphql';
-// export type Feed = {
-//   avatarLink: string;
-//   name: string;
-//   status: string;
-//   statusIcon: string;
-//   title: string;
-//   content: string;
-//   link?: string;
-//   isPinned: boolean;
-//   date: string;
-//   spotify?: string;
-//   image?: string;
-//   video?: string;
-//   posterVideo?: string;
-// };
+
 export type FeedProps = {
   feed: Feed;
 };
